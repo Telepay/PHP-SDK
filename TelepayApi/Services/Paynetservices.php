ea8b7924fbccc3240982a829aa0686d3352033bb
pere <pere@pasproduccions.com>
2015-01-22 21:10:49 +0100

    Added PaynetV2

--- TelepayApi/Services/Paynetservices.php
+++ TelepayApi/Services/Paynetservices.php
@@ -40,5 +40,42 @@ class Paynetservices extends BaseService {
         );
     }
 
+    public function infoV2($date,$hour,$transaction,$sku,$reference,$amount){
+        return $this->call(
+            'services/v2/paynet/payment/info',
+            array(),
+            'POST',
+            array(
+                'date'              =>  $date,
+                'hour'              =>  $hour,
+                'transaction_id'    =>  $transaction,
+                'sku'               =>  $sku,
+                'reference'         =>  $reference,
+                'amount'            =>  $amount
+            ),
+            array()
+        );
+    }
+
+    public function transactionV2($local_date,$local_hour,$transaction_id,$sku,$fee,$reference,$amount,$dv,$token){
+        return $this->call(
+            'services/v2/paynet/payment/transaction',
+            array(),
+            'POST',
+            array(
+                'date'          =>  $local_date,
+                'hour'          =>  $local_hour,
+                'transaction_id'=>  $transaction_id,
+                'sku'           =>  $sku,
+                'fee'           =>  $fee,
+                'reference'     =>  $reference,
+                'amount'        =>  $amount,
+                'dv'            =>  $dv,
+                'token'         =>  $token
+            ),
+            array()
+        );
+    }
+
 
 }
