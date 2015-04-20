b8d7ce9a4fd8186ae14009ac8cd0c7438fac9c71
pere <pere@pasproduccions.com>
2015-04-20 11:52:58 +0200

    Changed Safetypay call

--- src/TelepayApi/Services/Safetypay.php
+++ src/TelepayApi/Services/Safetypay.php
@@ -3,17 +3,16 @@ namespace TelepayApi\Services;
 
 class Safetypay extends BaseService {
 
-    public function request($request_date_time,$currency,$amount,$url_success,$url_error){
+    public function request($currency,$amount,$url_success,$url_error){
         return $this->call(
-            'services/v1/safetypay/payment/request',
+            'services/v1/safetypay',
             array(),
             'POST',
             array(
-                'date_time' =>  $request_date_time,
                 'currency'          =>  $currency,
                 'amount'            =>  $amount,
                 'url_success'       =>  $url_success,
-                'url_fail'         =>  $url_error
+                'url_fail'          =>  $url_error
             ),
             array()
         );
