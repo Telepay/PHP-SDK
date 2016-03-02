4f73e83b8ada9188b2a30d29f94beff3d56507e1
pere <pere@pasproduccions.com>
2016-03-02 17:43:17 +0100

    add tpv v2

--- src/TelepayApi/Services/PosService.php
+++ src/TelepayApi/Services/PosService.php
@@ -21,4 +21,23 @@ class PosService extends BaseService {
         );
     }
 
+    public function generateV2($amount, $description, $currency_in, $currency_out, $url_notification, $url_ok, $url_ko, $order_id, $id){
+        return $this->call(
+            'pos/v1/transaction/'.$id,
+            array(),
+            'POST',
+            array(
+                'amount'            =>  $amount,
+                'concept'           =>  $description,
+                'currency_in'       =>  $currency_in,
+                'currency_out'      =>  $currency_out,
+                'url_notification'  =>  $url_notification,
+                'url_ok'            =>  $url_ok,
+                'url_ko'            =>  $url_ko,
+                'order_id'          =>  $order_id
+            ),
+            array()
+        );
+    }
+
 }
